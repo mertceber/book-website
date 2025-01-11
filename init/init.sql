07b129abc861fa45cf20935ef6ea4f8f033aa9f6
Mert Ceber <mertceber134@gmail.com>
2025-01-11 21:40:44 +0300

    bug fix

--- init/init.sql
+++ init/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE bookDB;
-USE bookDB;
-CREATE TABLE Books (
+
+-- Veritabanı adı belirtildiği için 'USE bookDB' komutuna gerek yok
+CREATE TABLE bookDB.dbo.Books (
     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     Title NVARCHAR(MAX) NULL,
     Author NVARCHAR(MAX) NULL,
@@ -10,10 +11,10 @@ CREATE TABLE Books (
     DateRead DATETIME2 NULL
 );
 
-INSERT INTO Books ( Title, Description, Author, Rate, DateStart, DateRead)
+INSERT INTO bookDB.dbo.Books (Title, Description, Author, Rate, DateStart, DateRead)
 VALUES 
 ( 'Managing Oneself', 'We live in an age of unprecedented opportunity: with ambition, drive, and talent, you can rise to the top of your chosen profession, regardless of where you started out...', 'Peter Ducker', 4.9, '2019-01-20', NULL),
 ( 'Büyücü', 'Roman türünde bir kitap', 'John Fowles', 4.9, '2019-01-20', NULL),
-('1984', 'Roman türünde bir kitap', 'George Orwell', 4.9, '2019-01-20', NULL),
+( '1984', 'Roman türünde bir kitap', 'George Orwell', 4.9, '2019-01-20', NULL),
 ( 'Körlük', 'Roman türünde bir kitap', 'José Saramago', 4.9, '2019-01-20', NULL),
 ( 'Açlık', 'Roman türünde bir kitap', 'Knut Hamsun', 4.9, '2019-01-20', NULL);
